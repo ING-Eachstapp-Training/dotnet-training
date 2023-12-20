c8658d407798d95df9fc21f35fb7f3e51b0e417c
Yunus Ozturk <105066488+yunus-stapp@users.noreply.github.com>
2023-12-20 16:49:18 +0100

    Update TodoListDBContext.cs

--- chapter-4/Data/Context/TodoListDBContext.cs
+++ chapter-4/Data/Context/TodoListDBContext.cs
@@ -9,7 +9,7 @@ namespace chapter_4.Data.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=tcp:ing-training-sqlserver-dev.database.windows.net,1433;Initial Catalog=ing-training-db-dev;Persist Security Info=False;User ID=ing-admin;Password=yBTT05FVM70dTGGf;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("Server=tcp:ing-training-sqlserver-dev.database.windows.net,1433;Initial Catalog=ing-training;Persist Security Info=False;User ID=ing-admin;Password=yBTT05FVM70dTGGf;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
     }
 }
