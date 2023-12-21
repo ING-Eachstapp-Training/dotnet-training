8de9c772f00fdf2e0e5eb2d5d4eb5a5fbd707a8a
Yunus <yunus.ozturk@eachstapp.com>
2023-12-21 11:55:08 +0100

    add swagger security

--- chapter-4/Program.cs
+++ chapter-4/Program.cs
@@ -3,6 +3,7 @@ using chapter_4.BL;
 using chapter_4.DAL;
 using chapter_4.Data.Context;
 using chapter_4.Middleware;
+using Microsoft.OpenApi.Models;
 
 namespace chapter_4;
 
@@ -11,6 +12,36 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]
+                { }
+        }
+    });
+        });
 
         // Add services to the container.
 
